86575f4426fe11464dce4fec3b562d1102a37f3d
tang chuanjin <tcj044416@outlook.com>
2026-07-09 01:08:59 +0800

    Cache synthesis library page

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "mineral-evolution-v1";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icons/icon.svg"];
+const CACHE_NAME = "mineral-evolution-v2";
+const ASSETS = ["./", "./index.html", "./synthesis.html", "./manifest.json", "./icons/icon.svg"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
